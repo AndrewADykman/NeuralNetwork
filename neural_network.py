d78171bbaa3ce878e68d4cd1bbcff13690b41cda
AndrewADykman <AADykman@gmail.com>
2016-12-08 21:08:00 -0500

    changed other stuff

--- neural_network.py
+++ neural_network.py
@@ -4,12 +4,15 @@ from neural_layer import NeuralLayer
 class NeuralNetwork:
   def __init__(self, shape):
     self.num_layers = len(shape)
+    #might need to rework this
     self.network = [0.] * self.num_layers
     self.network[0] = NeuralLayer(shape[0], shape[0])
+    '''
     self.network[0].setAsInputLayer()
 
     for layer in range(1, self.num_layers):
       self.network[layer] = NeuralLayer(shape[layer-1], shape[layer])
+    '''
 
   def feedForward(self, inputs):
     for layer in range(self.num_layers):
@@ -20,3 +23,6 @@ class NeuralNetwork:
   def feedBack(self, guilt_vector):
     for layer in range(self.num_layers-1, -1, -1):
       guilt_vector = self.network[layer].backPropogate(guilt_vector)
+
+  def computerError(self, expected, returned):
+    return (expected - returned) ** 2
