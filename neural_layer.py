17d29f4721a6b8f6feb1a5a0b347b67cf3c51dac
AndrewADykman <AADykman@gmail.com>
2016-12-08 22:10:08 -0500

    commiting just for greg

--- neural_layer.py
+++ neural_layer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import logistic
 
 class NeuralLayer:
   def __init__(self, num_inputs, num_neurons):
@@ -6,20 +7,24 @@ class NeuralLayer:
     self.num_neurons = num_neurons
     self.potentials = np.zeros([self.num_neurons, 1])
     #change to have negative values
-    self.transform_matrix = np.random.rand(self.num_inputs, self.num_neurons)
+    self.weights = np.random.rand(self.num_inputs, self.num_neurons)
     self.bias = np.random.rand(self.num_neurons, 1)
+    self.Delta = np.zeros_like(self.potentials)
 
   def setPotentials(self, input_potentials):
-    self.potentials = np.dot(input_potentials, self.transform_matrix) + self.bias
+    self.potentials = np.dot(input_potentials, self.weights) + self.bias
 
   def getPotentials(self):
-    return self.potentials
+    return logistic(self.potentials)
 
-  def backPropogate(self, guilt_vector):
-    pass
-    #alter transform matrix
-    #create pass-back guilt vector
+  def backPropogate(self, delta_forward):
+    g_prime = self.derivOfLogistic()
+    delta = np.multiply(np.transpose(self.weights)*delta_forward, g_prime)
+    return delta
 
   def setAsInputLayer(self):
-    self.transform_matrix = np.identity(self.num_inputs)
+    self.weights = np.identity(self.num_inputs)
     self.bias = np.zeros([self.num_neurons, 1])
+
+  def derivOfLogistic(self):
+    return self.potentials(np.ones_like(self.potentials) - self.potentials)
