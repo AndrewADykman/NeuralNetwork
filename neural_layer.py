7f660217fc2195794ca041240972a67d502cdf8d
AndrewADykman <AADykman@gmail.com>
2016-12-08 20:55:48 -0500

    added bias

--- neural_layer.py
+++ neural_layer.py
@@ -5,17 +5,21 @@ class NeuralLayer:
     self.num_inputs = num_inputs
     self.num_neurons = num_neurons
     self.potentials = np.zeros([self.num_neurons, 1])
+    #change to have negative values
     self.transform_matrix = np.random.rand(self.num_inputs, self.num_neurons)
+    self.bias = np.random.rand(self.num_neurons, 1)
 
   def setPotentials(self, input_potentials):
-    self.potentials = np.dot(input_potentials, self.transform_matrix)
+    self.potentials = np.dot(input_potentials, self.transform_matrix) + self.bias
 
   def getPotentials(self):
     return self.potentials
 
   def backPropogate(self, guilt_vector):
+    pass
     #alter transform matrix
     #create pass-back guilt vector
 
   def setAsInputLayer(self):
-      self.transform_matrix = np.identity(num_inputs)
+    self.transform_matrix = np.identity(self.num_inputs)
+    self.bias = np.zeros([self.num_neurons, 1])
